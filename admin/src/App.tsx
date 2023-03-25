0ed7af03cec4a0a656671ab11aefa3fd77076b9f
Truong Gia Bao <ldcc.baotruong>
2023-03-25 21:34:38 +0700

    add admin FE

---
+++ admin/src/App.tsx
@@ -0,0 +1,22 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "definitions/chakra/theme";
+import "styles/global.scss";
+import { Provider } from "react-redux";
+import store from "redux/store";
+import "./i18n";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  return (
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <div className="App">
+          <Home />
+        </div>
+      </Provider>
+    </ChakraProvider>
+  );
+}
+
+export default App;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,13 @@ const Home: React.FC = () => {
   return (
     <Container>
       <Header />
+      User-service: Golang with Gin and mongodb: quản lý thông tin tài khoản người dùng, đăng ký, đăng nhập, quên mật khẩu, quản lý thông tin cá nhân
+
+Cart-service: Golang with Gin and redis: quản lý giỏ hàng của người dùng, thêm sản phẩm, xoá sản phẩm, chuyển sản phẩm giữa các trang
+
+Order-service: Golang with Gin and mongodb: quản lý đơn hàng, xem trạng thái đơn hàng, thanh toán đơn hàng, đặt hàng
+
+Về việc tách riêng lẽ các service, nếu bạn muốn nâng cao hiệu suất và quản lý được tốt hơn, tôi đề nghị bạn tách chúng thành các microservice riêng biệt, mỗi service có một nhiệm vụ nhất định và tương tác với các service khác thông qua các HTTP API.
       <Main />
       <Cards />
       <Footer />
